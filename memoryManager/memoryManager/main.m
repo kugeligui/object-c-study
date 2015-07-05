894cc26249d84b406978882c5062fbc8e306b2ff
ligui <kugeligui@qq.com>
2015-07-05 18:10:45 +0800

    创建内存管理项目

---
+++ memoryManager/memoryManager/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  memoryManager
+//
+//  Created by 李贵 on 15/7/5.
+//  Copyright (c) 2015年 李贵. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
